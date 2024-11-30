442c2d4299f50c7a2904fdb1019324cda8bbabdf
rakova21 <147162786+rakova21@users.noreply.github.com>
2024-11-30 12:28:03 +0300

    Add files via upload

---
+++ CrewRiskApp.java
@@ -0,0 +1,11 @@
+package com.crewrisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewRiskApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CrewRiskApp.class, args);
+    }
+}
